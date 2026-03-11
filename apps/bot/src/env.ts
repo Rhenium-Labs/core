308c4909a38cf348dba605403b426b355fca5d27
cobaltorum <216449108+cobaltorum@users.noreply.github.com>
2026-03-11 11:36:14 +0100

    refactor(apps/bot): Use t3-env for environment variable validation.

---
+++ apps/bot/src/env.ts
@@ -0,0 +1,39 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+	/**
+	 * Since the bot is just a single server app, we can just put all of our environment variables in the `server` section. The `client` section is meant for environment variables that are exposed to the client, which we don't have in this case.
+	 */
+	server: {
+		CLIENT_TOKEN: z.string().nonempty()
+	},
+
+	/**
+	 * The `client` section is meant for environment variables that are exposed to the client, which we don't have in this case. We still need to define it, but we can just leave it as an empty object.
+	 */
+	client: {},
+
+	/**
+	 * The prefix for environment variables that are exposed to the client. Since we don't have any client environment variables, we can just set this to something that won't conflict with our server environment variables.
+	 */
+	clientPrefix: "PUBLIC_",
+
+	/**
+	 * What object holds the environment variables at runtime.
+	 * In a Node.js environment, this is typically `process.env`.
+	 */
+	runtimeEnv: process.env,
+
+	/**
+	 * By default, this library will feed the environment variables directly to
+	 * the Zod validator.
+	 *
+	 * This means that if you have an empty string for a value that is supposed
+	 * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
+	 * it as a type mismatch violation. Additionally, if you have an empty string
+	 * for a value that is supposed to be a string with a default value (e.g.
+	 * `DOMAIN=` in an ".env" file), the default value will never be applied.
+	 */
+	emptyStringAsUndefined: true
+});
--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,6 +1,8 @@
 // How does Node still not load .env files by default.
 import "dotenv/config.js";
 
+import { env } from "./env.js";
+
 import Rhenium from "#structures/Rhenium.js";
 
 /** Primary discord.js client instance. */
@@ -8,14 +10,11 @@ export const client = new Rhenium();
 
 /** Main entry point for the program. */
 async function main(): Promise<void> {
-	if (!process.env.CLIENT_TOKEN)
-		throw new Error("The 'CLIENT_TOKEN' environment variable is not set.");
-
 	// Mount event listeners.
 	await client.mountEventListeners();
 
 	// Connect to the gateway.
-	await client.login(process.env.CLIENT_TOKEN);
+	await client.login(env.CLIENT_TOKEN);
 }
 
 void main();
